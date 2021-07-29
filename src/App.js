ab217601de589b9422c0e686ee9e77821509930b
Adam Gibson <agibson@mcw.edu>
2021-07-29 11:35:32 -0500

    Installed Prime React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import React, { useState, useRef } from 'react';
+
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import PrimeReact from 'primereact/api';
+import { Sidebar } from 'primereact/sidebar';
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const toastRef = useRef();
+  const [visible, setVisible] = useState(true);
+
+
+  // active ripple effect
+  PrimeReact.ripple = true;
+
+  const onFormSubmit = (e) => {
+    if (text) {
+      toastRef.current.show({ severity: 'info', summary: text, life: 3000 });
+    }
+
+    // clear
+    setText('');
+
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
+      <Sidebar visible={visible} position="left" onHide={() => setVisible(false)}>
+         Content
+      </Sidebar>
+      <Toast ref={toastRef} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <form className="p-d-flex p-jc-center p-mt-6" onSubmit={onFormSubmit}>
+        <InputText value={text} onChange={(e) => setText(e.target.value)} />
+        <Button type="submit" label="Submit" icon="pi pi-check" className="p-ml-2"/>
+      </form>
     </div>
   );
 }
